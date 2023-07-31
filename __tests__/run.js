c08a6e51656ad8aa6e77f3ab5e042c2a4540abb9
zero.li <zeroliql@gmail.com>
2023-07-31 01:07:43 -0700

    revert one change to have all tests

--- __tests__/run.js
+++ __tests__/run.js
@@ -3,17 +3,17 @@ const Eva = require('../Eva');
 const Environment = require('../Environment');
 
 const tests = [
-    // require('./self-eval-test.js'),
-    // require('./math-test.js'),
-    // require('./variables-test.js'),
-    // require('./block-test.js'),
-    // require('./if-test.js'),
-    // require('./while-test.js'),
-    // require('./built-in-function-test.js'),
-    // require('./user-defined-function-test.js'),
-    // require('./lambda-function-test.js'),
-    // require('./switch-test.js'),
-    // require('./for-test.js'),
+    require('./self-eval-test.js'),
+    require('./math-test.js'),
+    require('./variables-test.js'),
+    require('./block-test.js'),
+    require('./if-test.js'),
+    require('./while-test.js'),
+    require('./built-in-function-test.js'),
+    require('./user-defined-function-test.js'),
+    require('./lambda-function-test.js'),
+    require('./switch-test.js'),
+    require('./for-test.js'),
     require('./class-test.js'),
 ];
 
